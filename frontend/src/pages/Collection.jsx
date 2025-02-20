39ecf3daf76e1e3d72ae82902aba36424a212341
Sakib548 <mdsakibhassan548@gmail.com>
2025-02-20 17:43:38 +0600

    Filter Options

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { assets } from "../assets/assets/frontend_assets/assets";
+import Title from "../components/Title";
 import { ShopContext } from "../context/ShopContext";
 
 const Collection = () => {
@@ -9,7 +10,10 @@ const Collection = () => {
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
       {/* Filter Options */}
       <div className="min-w-60">
-        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+        <p
+          onClick={() => setShowFilter(!showFilter)}
+          className="my-2 text-xl flex items-center cursor-pointer gap-2"
+        >
           FILTERS
           <img
             className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
@@ -60,6 +64,19 @@ const Collection = () => {
               Winterwear
             </p>
           </div>
+        </div>
+      </div>
+
+      {/* Right Side */}
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
+          <Title text1={"ALL"} text2={"COLLECTIONS"} />
+          {/* Product sort */}
+          <select className="border-2 border-gray-300 text-sm px-2">
+            <option value="relevant">Sort by Relavent</option>
+            <option value="low-high">Sort by Low to High</option>
+            <option value="high-low">Sort by High to Low</option>
+          </select>
         </div>
       </div>
     </div>
