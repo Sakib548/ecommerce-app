305bd9350010fbabe2fd8da7f04cabbfe363c4c3
Sakib548 <mdsakibhassan548@gmail.com>
2025-02-24 15:47:49 +0600

    started working on single product

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,4 +1,28 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+
 const Product = () => {
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+
+  const fetchProductData = async () => {
+    let product = products.filter((product) => product._id === productId);
+    setProductData(product);
+    // products.map((item) => {
+    //   if (item._id === productId) {
+    //     setProductData(item);
+    //     console.log(item);
+    //     return null;
+    //   }
+    // });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId]);
+  console.log(productData);
   return <div>Product</div>;
 };
 export default Product;
